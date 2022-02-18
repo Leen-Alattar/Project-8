178a9daa40abdc2d9715f7b223e3d585b1f18c16
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-18 17:26:19 +0200

    adding route and error page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-
-import { useState } from 'react';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Booking from "./components/Booking/Booking";
+import Error from "./components/Error404/Error";
 function App() {
-
-  
   return (
-   <><div>hi</div></>
+    <>
+      <div>hi</div>
+      <Routes>
+        <Route path="booking" element={<Booking />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </>
   );
 }
 
