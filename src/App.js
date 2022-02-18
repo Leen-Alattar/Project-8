69bb3222d20a192446b8eab7f95ab3c4410b2b70
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-18 17:41:47 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Booking from "./components/Booking/Booking";
 import Error from "./components/Error404/Error";
+import "./style/style.css";
 function App() {
   return (
     <>
