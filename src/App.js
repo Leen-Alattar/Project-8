e77b3bb9fdacb74e299fc388cb656f207ce99446
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-19 19:06:28 +0200

    all table json

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Booking from "./components/Booking/Booking";
 import Error from "./components/Error404/Error";
-import {Header} from './components/Header/Header.jsx'
-import {Menu} from './components/Menu/Menu'
+import { Header } from "./components/Header/Header.jsx";
+import { Menu } from "./components/Menu/Menu";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<Header/>}></Route>
+        <Route path="/" element={<Menu />}></Route>
         <Route path="booking" element={<Booking />}></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
-<Menu/>
-
     </>
   );
 }
