94b7ee1fb9f2a4ed6cad114fe884eadb826b29c2
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-21 00:31:00 +0200

    animation filter

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Buttons from '../../Buttons';
-import Card from '../../Card';
+import React from "react";
+import Buttons from "../../Buttons";
+import Card from "../../Card";
 import Data from "../../Data";
-const Filter = ({item ,setItem}) => {
-    const menuItems = [...new Set(Data.map((Val) => Val.category))];
+
+const Filter = ({ item, setItem }) => {
+  const menuItems = [...new Set(Data.map((Val) => Val.category))];
 
   const filterItem = (curcat) => {
     const newItem = Data.filter((newVal) => {
@@ -11,9 +12,10 @@ const Filter = ({item ,setItem}) => {
     });
     setItem(newItem);
   };
-    return ( <>
-     {/* filter  */}
-     <div className="container3">
+  return (
+    <>
+      {/* filter  */}
+      <div className="container3">
         <div className="row3">
           <h1 className="col-12 text-center my-3 fw-bold head">Our Menu</h1>
           <Buttons
@@ -24,8 +26,8 @@ const Filter = ({item ,setItem}) => {
           <Card item={item} />
         </div>
       </div>
-    
-    </> );
-}
- 
+    </>
+  );
+};
+
 export default Filter;
