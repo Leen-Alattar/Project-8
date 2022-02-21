00166039614c0db7b746b9b4b76748dcd330fdbc
haithamassoli <haitham.b.assoli@gmail.com>
2022-02-21 14:44:56 +0200

    fix go to top

--- src/components/Land/land.js
+++ src/components/Land/land.js
@@ -1,119 +1,163 @@
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export const Land=()=>{
-
-return(
-<>
-<sdiv>
-<div id="banner" class="banner full-screen-mode parallax">
-        <div class="container pr">
+export const Land = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <sdiv>
+        <div id="banner" class="banner full-screen-mode parallax">
+          <div class="pr container">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="banner-static">
-                    <div class="banner-text">
-                       <div class="banner-cell"  style={{ paddingBottom:'210px' }}>
-                            <h1>Dinner with us   <span class="typer" id="some-id" data-delay="200" data-delim=":" data-words="Friends:Family:Officemates" data-colors="red"></span><span class="cursor" data-cursorDisplay="_" data-owner="some-id"></span></h1>
-                            <h2>Accidental appearances </h2>
-                            <div class="book-btn">
-                                <Link to="/booking" class="table-btn hvr-underline-from" style={{ marginLeft:'20px' }}>Book my Table</Link>
-                            </div>
-                            <a href="#about">
-                            </a>
-                        </div>
+              <div class="banner-static">
+                <div class="banner-text">
+                  <div class="banner-cell" style={{ paddingBottom: "210px" }}>
+                    <h1>
+                      Dinner with us{" "}
+                      <span
+                        class="typer"
+                        id="some-id"
+                        data-delay="200"
+                        data-delim=":"
+                        data-words="Friends:Family:Officemates"
+                        data-colors="red"
+                      ></span>
+                      <span
+                        class="cursor"
+                        data-cursorDisplay="_"
+                        data-owner="some-id"
+                      ></span>
+                    </h1>
+                    <h2>Accidental appearances </h2>
+                    <div class="book-btn">
+                      <Link
+                        to="/booking"
+                        class="table-btn hvr-underline-from"
+                        style={{ marginLeft: "20px" }}
+                      >
+                        Book my Table
+                      </Link>
                     </div>
+                    <a href="#about"></a>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
 
-
-
-    <div class="special-menu pad-top-100 parallax">
-        <div class="container">
+        <div class="special-menu pad-top-100 parallax">
+          <div class="container">
             <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
-                        <h2 class="block-title color-white text-center"> Today's Special </h2>
-                    </div>
-                    <div class="special-box">
-                        <div id="owl-demo">
-                            <div class="item item-type-zoom">
-                                
-                                <div class="item-img">
-                                    <img src="images/special-menu-1.jpg" alt="sp-menu"/>
-                                </div>
-                            </div>
-                            <div class="item item-type-zoom">
-                               
-                                <div class="item-img">
-                                    <img src="images/special-menu-2.jpg" alt="sp-menu"/>
-                                </div>
-                            </div>
-                            <div class="item item-type-zoom">
-                               
-                                <div class="item-img">
-                                    <img src="images/special-menu-3.jpg" alt="sp-menu"/>
-                                </div>
-                            </div>
-                            <div class="item item-type-zoom">
-                            
-                                <div class="item-img">
-                                    <img src="images/special-menu-1.jpg" alt="sp-menu"/>
-                                </div>
-                            </div>
-                            <div class="item item-type-zoom">
-                               
-                                <div class="item-img">
-                                    <img src="images/special-menu-2.jpg" alt="sp-menu"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div
+                  class="wow fadeIn"
+                  data-wow-duration="1s"
+                  data-wow-delay="0.1s"
+                >
+                  <h2 class="block-title color-white text-center">
+                    {" "}
+                    Today's Special{" "}
+                  </h2>
                 </div>
+                <div class="special-box">
+                  <div id="owl-demo">
+                    <div class="item item-type-zoom">
+                      <div class="item-img">
+                        <img src="images/special-menu-1.jpg" alt="sp-menu" />
+                      </div>
+                    </div>
+                    <div class="item item-type-zoom">
+                      <div class="item-img">
+                        <img src="images/special-menu-2.jpg" alt="sp-menu" />
+                      </div>
+                    </div>
+                    <div class="item item-type-zoom">
+                      <div class="item-img">
+                        <img src="images/special-menu-3.jpg" alt="sp-menu" />
+                      </div>
+                    </div>
+                    <div class="item item-type-zoom">
+                      <div class="item-img">
+                        <img src="images/special-menu-1.jpg" alt="sp-menu" />
+                      </div>
+                    </div>
+                    <div class="item item-type-zoom">
+                      <div class="item-img">
+                        <img src="images/special-menu-2.jpg" alt="sp-menu" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+      </sdiv>
 
-
-</sdiv>
-
-<div id="about" class="about-main pad-top-100 pad-bottom-100">
+      <div id="about" class="about-main pad-top-100 pad-bottom-100">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
-                        <h2 class="block-title"> About Us </h2>
-                        <h3>IT STARTED, QUITE SIMPLY, LIKE THIS...</h3>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm incididunt ut labore et dolore magna aliqua. Ut enim ad minim venia, nostrud exercitation ullamco. </p>
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div
+                class="wow fadeIn"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <h2 class="block-title"> About Us </h2>
+                <h3>IT STARTED, QUITE SIMPLY, LIKE THIS...</h3>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusm incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim venia, nostrud exercitation ullamco.{" "}
+                </p>
 
-                        <p> Aenean commodo ligula eget dolor aenean massa. Cum sociis nat penatibu set magnis dis parturient montes, nascetur ridiculus mus. quam felisorat, ultricies nec, Aenean commodo ligula eget dolor penatibu set magnis is parturient montes, nascetur ridiculus mus. quam felisorat, ultricies nec, pellentesque eu, pretium quis, sem. quat massa quis enim. Donec vitae sapien ut libero venenatis fauci Nullam quis ante. Etiam sit amet rci eget eros. </p>
+                <p>
+                  {" "}
+                  Aenean commodo ligula eget dolor aenean massa. Cum sociis nat
+                  penatibu set magnis dis parturient montes, nascetur ridiculus
+                  mus. quam felisorat, ultricies nec, Aenean commodo ligula eget
+                  dolor penatibu set magnis is parturient montes, nascetur
+                  ridiculus mus. quam felisorat, ultricies nec, pellentesque eu,
+                  pretium quis, sem. quat massa quis enim. Donec vitae sapien ut
+                  libero venenatis fauci Nullam quis ante. Etiam sit amet rci
+                  eget eros.{" "}
+                </p>
 
-                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm incididunt ut labore et dolore magna aliqua. Ut enim ad minim venia, nostrud exercitation ullamco. </p>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
-                        <div class="about-images">
-                            <img class="about-main" src="images/about-main.jpg" alt="About Main Image"/>
-                            <img class="about-inset" src="images/about-inset.jpg" alt="About Inset Image"/>
-                        </div>
-                    </div>
-                </div>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusm incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim venia, nostrud exercitation ullamco.{" "}
+                </p>
+              </div>
             </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div
+                class="wow fadeIn"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <div class="about-images">
+                  <img
+                    class="about-main"
+                    src="images/about-main.jpg"
+                    alt="About Main Image"
+                  />
+                  <img
+                    class="about-inset"
+                    src="images/about-inset.jpg"
+                    alt="About Inset Image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-
-                        
-
-
-
-
-
-
-
-</>
-
-
-)
-
-
-}
+      </div>
+    </>
+  );
+};
